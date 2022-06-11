513f9637f30c9bbe138cc0956edf115c204f8f83
DELL <anjugupta7060@gmail.com>
2022-06-11 09:11:21 +0530

    Donation App UI

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,166 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white30,
+        elevation: 0,
+        centerTitle: true,
+        title: Text(
+          "Home",
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 20,
+            fontWeight: FontWeight.w600,
+            letterSpacing: 1.2,
+          ),
+        ),
+        leading: Icon(
+          Icons.menu,
+          color: Colors.black,
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 15.0),
+            child: Icon(
+              Icons.filter,
+              color: Colors.black,
+            ),
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(left: 15, right: 15, top: 15),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Container(
+                child: GridView.count(
+                  crossAxisCount: 2,
+                  shrinkWrap: true,
+                  childAspectRatio: 2.9 / 1,
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 14,
+                  children: [
+                    gridViewItem(Color.fromRGBO(4, 119, 204, 1.0),
+                        Icons.fire_extinguisher, "Popularity"),
+                    gridViewItem(Color.fromRGBO(68, 37, 168, 1.0),
+                        Icons.calendar_today, "End Date"),
+                    gridViewItem(Color.fromRGBO(192, 43, 59, 1.0),
+                        Icons.video_collection, "Newest"),
+                    gridViewItem(Color.fromRGBO(243, 182, 48, 1.0),
+                        Icons.money_off, "Most Funded"),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 35,
+              ),
+              Container(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  "Recommended",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 28,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 28,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 20.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  child: Column(
+                    children: [
+                      Stack(
+                        children: [
+                          Container(
+                            height: MediaQuery.of(context).size.height / 2,
+                            width: MediaQuery.of(context).size.width,
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(20),
+                              child: Image(
+                                fit: BoxFit.cover,
+                                image: NetworkImage(
+                                    "https://i.pinimg.com/originals/51/cd/ec/51cdec800c9a1f230b318b5bba8942ff.jpg"),
+                              ),
+                            ),
+                          ),
+                          Container(
+                            child: Padding(
+                              padding:
+                                  const EdgeInsets.only(left: 18.0, top: 24),
+                              child: Text(
+                                "Kids Education",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 28,
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 36,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget gridViewItem(color, icon, text) {
+    return Container(
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(36),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            icon,
+            color: Colors.white,
+            size: 30,
+          ),
+          SizedBox(
+            width: 4,
+          ),
+          Text(
+            text,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 18,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
